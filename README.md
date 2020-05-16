39d12af14d0bf1a8b7eba41efeeb3c348a545292
Labarre Gautier <gautier@labarre.me>
2020-05-16 22:19:54 +0200

    New name for Gnome MPV -> Celluloid Also code reformat and doc revamped

--- README.md
+++ README.md
@@ -1,30 +1,34 @@
-# Gnome MPV remote
-This repository contains a unified remote to control your Gnome MPV software.
+# Celluloid remote
+This repository contains a unified remote to control your Celluloid software.
 
 With this you can :
 
 - Start / Pause
 - Stop and save (position saved on the video)
-- Stop and close gnome MPV
+- Stop and close celluloid
 - Previous/Next video on playlist 
 - Set the video in fullscreen
 - Reduce/Increase volume
 - Mute
-- Go back and forward (around 15seconds)
+- Go back and forward (around 15 seconds)
 - Show/Hide subtitles
 
 
 ## Installation
 
-Once you unzip the folder, open the unified remote server, go on settings, then remotes.
-On this page, you can add locations where unified remote will look for remotes. Add the Path of this project. Save and restart the server.
+- unzip the folder, 
+- open the unified remote server, 
+- go on settings, then remotes.
 
-You should see "GNOME MPV
-Simple remote to handle Gnome MPV shortcuts." on the page "Remotes".
+On this page, you can add locations where unified remote will look for remotes. 
+- Add the Path of this project.
+- Save and restart the server.
+
+You should see "Celluloid Simple remote to handle Celluloid shortcuts." on the page "Remotes".
 
 Then on your phone, go to "Get More" and "Media", then add the remote.
 
 
 ## Other
 
-Get Gnome MPV on [Gnome MPV website](https://gnome-mpv.github.io)
+Get Celluloid on [Gnome MPV website](https://celluloid-player.github.io/)
--- remote.lua
+++ remote.lua
@@ -1,66 +1,62 @@
+-- Gnome mpv / Celluloid unified remote
+-- @author Gautier LABARRE
+
 local kb = libs.keyboard;
 
--- Documentation
--- http://www.unifiedremote.com/api
-
--- Keyboard Library
--- http://www.unifiedremote.com/api/libs/keyboard
-
-
 --@help First line : Play/Pause / Stop and save position / Stop and quit
-actions.playpause = function ()
-	kb.stroke("P");
+actions.playpause = function()
+    kb.stroke("P");
 end
 
-actions.stopAndSave = function ()
-	kb.stroke("shift", "Q");
+actions.stopAndSave = function()
+    kb.stroke("shift", "Q");
 end
 
-actions.stop = function ()
-	kb.stroke("Q");
+actions.stop = function()
+    kb.stroke("Q");
 end
 
 --@help Second line : Previous / FullScreen / Next 
-actions.previous = function ()
-	kb.stroke("mediaprevious");
+actions.previous = function()
+    kb.stroke("mediaprevious");
 end
 
-actions.fullscreen = function ()
-	kb.stroke("F");
+actions.fullscreen = function()
+    kb.stroke("F");
 end
 
-actions.next = function ()
-	kb.stroke("medianext");
+actions.next = function()
+    kb.stroke("medianext");
 end
 
 --@help Third line : volume down / Mute / volume up 
 
-actions.volumeDown = function ()
-	kb.stroke("shift", "9");
+actions.volumeDown = function()
+    kb.stroke("shift", "9");
 end
 
-actions.mute = function ()
-	kb.stroke("M");
+actions.mute = function()
+    kb.stroke("M");
 end
 
-actions.volumeUp = function ()
-	kb.stroke("shift", "0");
+actions.volumeUp = function()
+    kb.stroke("shift", "0");
 end
 
 --@help Fourth line : +-5s / Subtitles on and off / +15s 
-actions.jumpBack = function ()
-	kb.stroke("left");
+actions.jumpBack = function()
+    kb.stroke("left");
 end
 
-actions.toggleSubtitles = function ()
-	kb.stroke("V");
+actions.toggleSubtitles = function()
+    kb.stroke("V");
 end
 
-actions.togglePlaylist = function ()
-	kb.stroke("F9");
+actions.togglePlaylist = function()
+    kb.stroke("F9");
 end
 
-actions.jumbForward = function ()
-	kb.stroke("right");
+actions.jumbForward = function()
+    kb.stroke("right");
 end
 
